45059c15b4b3bfe12057aeb701168ace1b125a27
DaisukeShinoku <shinoku.daisuke.ycu@gmail.com>
2021-06-20 13:56:05 +0900

    初期データ設定

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -1,7 +1,253 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+users = [
+  {
+    id: 1,
+    email: "user01@test.com",
+    name: "地雷子",
+    image: open("./db/fixtures/users/01.jpeg"),
+    gender: 1,
+    birthday: "1999-01-01",
+    profile: "地雷子です。どうぞよろしくお願いします。",
+    prefecture: 13,
+  },
+  {
+    id: 2,
+    email: "user02@test.com",
+    name: "アイドル子",
+    image: open("./db/fixtures/users/02.jpeg"),
+    gender: 1,
+    birthday: "2000-06-11",
+    profile: "アイドル子です。どうぞよろしくお願いします。",
+    prefecture: 13,
+  },
+  {
+    id: 3,
+    email: "user03@test.com",
+    name: "占い子",
+    image: open("./db/fixtures/users/03.jpeg"),
+    gender: 1,
+    birthday: "1993-11-02",
+    profile: "占い子です。どうぞよろしくお願いします。",
+    prefecture: 13,
+  },
+  {
+    id: 4,
+    email: "user04@test.com",
+    name: "ほろ酔い子",
+    image: open("./db/fixtures/users/04.jpeg"),
+    gender: 1,
+    birthday: "1995-02-13",
+    profile: "ほろ酔い子です。どうぞよろしくお願いします。",
+    prefecture: 13,
+  },
+  {
+    id: 5,
+    email: "user05@test.com",
+    name: "メンヘラ子",
+    image: open("./db/fixtures/users/05.jpeg"),
+    gender: 1,
+    birthday: "1993-02-01",
+    profile: "メンヘラ子です。どうぞよろしくお願いします。",
+    prefecture: 13,
+  },
+  {
+    id: 6,
+    email: "user06@test.com",
+    name: "山ガール子",
+    image: open("./db/fixtures/users/06.jpeg"),
+    gender: 1,
+    birthday: "1997-01-20",
+    profile: "山ガール子です。どうぞよろしくお願いします。",
+    prefecture: 13,
+  },
+  {
+    id: 7,
+    email: "user07@test.com",
+    name: "ゲーム子",
+    image: open("./db/fixtures/users/07.jpeg"),
+    gender: 1,
+    birthday: "1999-12-10",
+    profile: "ゲーム子です。どうぞよろしくお願いします。",
+    prefecture: 14,
+  },
+  {
+    id: 8,
+    email: "user08@test.com",
+    name: "スポーツ子",
+    image: open("./db/fixtures/users/08.jpeg"),
+    gender: 1,
+    birthday: "1995-01-03",
+    profile: "スポーツ子です。どうぞよろしくお願いします。",
+    prefecture: 14,
+  },
+  {
+    id: 9,
+    email: "user09@test.com",
+    name: "キャバ子",
+    image: open("./db/fixtures/users/09.jpeg"),
+    gender: 1,
+    birthday: "1997-11-11",
+    profile: "キャバ子です。どうぞよろしくお願いします。",
+    prefecture: 14,
+  },
+  {
+    id: 10,
+    email: "user10@test.com",
+    name: "グラドル子",
+    image: open("./db/fixtures/users/10.jpeg"),
+    gender: 1,
+    birthday: "1993-03-03",
+    profile: "グラドル子です。どうぞよろしくお願いします。",
+    prefecture: 14,
+  },
+  {
+    id: 11,
+    email: "user11@test.com",
+    name: "考え男",
+    image: open("./db/fixtures/users/11.jpeg"),
+    gender: 0,
+    birthday: "1992-01-11",
+    profile: "考え男です。どうぞよろしくお願いします。",
+    prefecture: 13,
+  },
+  {
+    id: 12,
+    email: "user12@test.com",
+    name: "ニート男",
+    image: open("./db/fixtures/users/12.jpeg"),
+    gender: 0,
+    birthday: "1992-01-11",
+    profile: "ニート男です。どうぞよろしくお願いします。",
+    prefecture: 13,
+  },
+  {
+    id: 13,
+    email: "user13@test.com",
+    name: "ギタ男",
+    image: open("./db/fixtures/users/13.jpeg"),
+    gender: 0,
+    birthday: "1992-01-11",
+    profile: "ギタ男です。どうぞよろしくお願いします。",
+    prefecture: 13,
+  },
+  {
+    id: 14,
+    email: "user14@test.com",
+    name: "ビジ男",
+    image: open("./db/fixtures/users/14.jpeg"),
+    gender: 0,
+    birthday: "1992-01-11",
+    profile: "ビジ男です。どうぞよろしくお願いします。",
+    prefecture: 13,
+  },
+  {
+    id: 15,
+    email: "user15@test.com",
+    name: "カメアシ男",
+    image: open("./db/fixtures/users/15.jpeg"),
+    gender: 0,
+    birthday: "1992-01-11",
+    profile: "カメアシ男です。どうぞよろしくお願いします。",
+    prefecture: 13,
+  },
+  {
+    id: 16,
+    email: "user16@test.com",
+    name: "ウバ男",
+    image: open("./db/fixtures/users/16.jpeg"),
+    gender: 0,
+    birthday: "1992-01-11",
+    profile: "ウバ男です。どうぞよろしくお願いします。",
+    prefecture: 13,
+  },
+  {
+    id: 17,
+    email: "user17@test.com",
+    name: "格闘男",
+    image: open("./db/fixtures/users/17.jpeg"),
+    gender: 0,
+    birthday: "1992-01-11",
+    profile: "格闘男です。どうぞよろしくお願いします。",
+    prefecture: 14,
+  },
+  {
+    id: 18,
+    email: "user18@test.com",
+    name: "感染予防男",
+    image: open("./db/fixtures/users/18.jpeg"),
+    gender: 0,
+    birthday: "1992-01-11",
+    profile: "感染予防男です。どうぞよろしくお願いします。",
+    prefecture: 14,
+  },
+  {
+    id: 19,
+    email: "user19@test.com",
+    name: "竹刀男",
+    image: open("./db/fixtures/users/19.jpeg"),
+    gender: 0,
+    birthday: "1992-01-11",
+    profile: "竹刀男です。どうぞよろしくお願いします。",
+    prefecture: 14,
+  },
+  {
+    id: 20,
+    email: "user20@test.com",
+    name: "メガネ男",
+    image: open("./db/fixtures/users/20.jpeg"),
+    gender: 0,
+    birthday: "1992-01-11",
+    profile: "メガネ男です。どうぞよろしくお願いします。",
+    prefecture: 14,
+  }
+]
+
+puts "Start creating users..."
+
+users.each do |user|
+  User.create(
+    email: user[:email],
+    password: "password",
+    name: user[:name],
+    image: user[:image],
+    gender: user[:gender],
+    birthday: user[:birthday],
+    profile: user[:profile],
+    prefecture: user[:prefecture],
+  )
+end
+
+puts "Completed!"
+
+likes = [
+  {
+    from_user_id: 1,
+    to_user_id: 11,
+  },
+  {
+    from_user_id: 13,
+    to_user_id: 3,
+  },
+  {
+    from_user_id: 4,
+    to_user_id: 14,
+  },
+  {
+    from_user_id: 15,
+    to_user_id: 5,
+  },
+  {
+    from_user_id: 6,
+    to_user_id: 16,
+  }
+]
+
+puts "Start creating likes..."
+
+likes.each do |like|
+  Like.create(
+    from_user_id: like[:from_user_id],
+    to_user_id: like[:to_user_id]
+  )
+end
+
+puts "Completed!"
